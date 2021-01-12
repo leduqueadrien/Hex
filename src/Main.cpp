8bc375fabee4fc714c4515ccc1e45647c45ea2cf
Cavani Nicolas <cavani.nicolas6@gmail.com>
2021-01-12 16:40:41 +0100

    add closable window

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,35 +4,44 @@
 
 int main(int argc, char** argv)
 {
-    /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
-    {
-        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
-        return -1;
-    }
+	/* Initialisation simple */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+	{
+		fprintf(stdout, "Échec de l'initialisation de la SDL (%s)\n", SDL_GetError());
+		return -1;
+	}
 
-    {
-        /* Création de la fenêtre */
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                                  640,
-                                                                  480,
-                                                                  SDL_WINDOW_SHOWN);
+	bool running = true;
+	SDL_Event event;
 
-        if( pWindow )
-        {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+	/* Création de la fenêtre */
+	SDL_Window* pWindow = NULL;
+	pWindow = SDL_CreateWindow("Ma première application SDL2", SDL_WINDOWPOS_UNDEFINED,
+																SDL_WINDOWPOS_UNDEFINED,
+																640,
+																480,
+																SDL_WINDOW_SHOWN);
 
-            SDL_DestroyWindow(pWindow);
-        }
-        else
-        {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        }
-    }
+	if (!pWindow) {
+		fprintf(stderr, "Erreur de création de la fenêtre: %s\n", SDL_GetError());
+		running = false;
+	}
 
-    SDL_Quit();
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+		SDL_Delay(16);
+	}
 
-    return 0;
+
+	SDL_DestroyWindow(pWindow);
+	SDL_Quit();
+
+	return 0;
 }
