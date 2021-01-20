6370953c4316ae5f2a5ef04bbde80b1f996b64a6
Cavani Nicolas <cavani.nicolas6@gmail.com>
2021-01-20 20:20:11 +0100

    add drawHexagon()

--- src/test.cpp
+++ src/test.cpp
@@ -8,10 +8,10 @@
 #define POINTS_COUNT 4
 
 static SDL_Point points[POINTS_COUNT] = {
-    {320, 200},
-    {300, 240},
-    {340, 240},
-    {320, 200}
+	{320, 200},
+	{300, 240},
+	{340, 240},
+	{320, 200}
 };
 
 static SDL_Point points2[5000];
@@ -27,103 +27,136 @@ void putpixel(int x0, int y0, int i) {
 
 void DrawCircle(int x0, int y0, int radius)
 {
-    int x = 0, y = radius;
-    int dp = 1 - radius;
-    do
-    {
-        if (dp < 0)
-            dp = dp + 2 * (++x) + 3;
-        else
-            dp = dp + 2 * (++x) - 2 * (--y) + 5;
+	int x = 0, y = radius;
+	int dp = 1 - radius;
+	do
+	{
+		if (dp < 0)
+			dp = dp + 2 * (++x) + 3;
+		else
+			dp = dp + 2 * (++x) - 2 * (--y) + 5;
 
-        putpixel(x0 + x, y0 + y, 15); //For the 8 octants
-        putpixel(x0 - x, y0 + y, 15);
-        putpixel(x0 + x, y0 - y, 15);
-        putpixel(x0 - x, y0 - y, 15);
-        putpixel(x0 + y, y0 + x, 15);
-        putpixel(x0 - y, y0 + x, 15);
-        putpixel(x0 + y, y0 - x, 15);
-        putpixel(x0 - y, y0 - x, 15);
+		putpixel(x0 + x, y0 + y, 15); //For the 8 octants
+		putpixel(x0 - x, y0 + y, 15);
+		putpixel(x0 + x, y0 - y, 15);
+		putpixel(x0 - x, y0 - y, 15);
+		putpixel(x0 + y, y0 + x, 15);
+		putpixel(x0 - y, y0 + x, 15);
+		putpixel(x0 + y, y0 - x, 15);
+		putpixel(x0 - y, y0 - x, 15);
 
-    } while (x < y);
+	} while (x < y);
 }
 
 
-void MyDrawCircle(SDL_Renderer* renderer,int x0, int y0, int radius)
+void MyDrawCircle(SDL_Renderer* renderer, int x0, int y0, int radius)
 {
-    int x = 0, y = radius;
-    int dp = 1 - radius;
-    do
-    {
-        if (dp < 0)
-            dp = dp + 2 * (++x) + 3;
-        else
-            dp = dp + 2 * (++x) - 2 * (--y) + 5;
+	int x = 0, y = radius;
+	int dp = 1 - radius;
+	do
+	{
+		if (dp < 0)
+			dp = dp + 2 * (++x) + 3;
+		else
+			dp = dp + 2 * (++x) - 2 * (--y) + 5;
 
-        SDL_RenderDrawPoint(renderer, x0 + x, y0 + y); //For the 8 octants
-        SDL_RenderDrawPoint(renderer, x0 - x, y0 + y);
-        SDL_RenderDrawPoint(renderer, x0 + x, y0 - y);
-        SDL_RenderDrawPoint(renderer, x0 - x, y0 - y);
-        SDL_RenderDrawPoint(renderer, x0 + y, y0 + x);
-        SDL_RenderDrawPoint(renderer, x0 - y, y0 + x);
-        SDL_RenderDrawPoint(renderer, x0 + y, y0 - x);
-        SDL_RenderDrawPoint(renderer, x0 - y, y0 - x);
+		SDL_RenderDrawPoint(renderer, x0 + x, y0 + y); //For the 8 octants
+		SDL_RenderDrawPoint(renderer, x0 - x, y0 + y);
+		SDL_RenderDrawPoint(renderer, x0 + x, y0 - y);
+		SDL_RenderDrawPoint(renderer, x0 - x, y0 - y);
+		SDL_RenderDrawPoint(renderer, x0 + y, y0 + x);
+		SDL_RenderDrawPoint(renderer, x0 - y, y0 + x);
+		SDL_RenderDrawPoint(renderer, x0 + y, y0 - x);
+		SDL_RenderDrawPoint(renderer, x0 - y, y0 - x);
 
-    } while (x < y);
+	} while (x < y);
 }
 
+void drawCircles(SDL_Renderer * renderer)
+{
+	SDL_RenderDrawLine(renderer, 50, 250, 450, 250); //barre horizontale
+	SDL_RenderDrawLine(renderer, 250, 50, 250, 450); //barre verticale
+	for (int j=0; j<200; j++) {
+		SDL_SetRenderDrawColor(renderer, 50*j%255, 30*j%255, 35*j%255, SDL_ALPHA_OPAQUE);
+		MyDrawCircle(renderer, 250, 250, j);
+	}
+}
+
+void drawHexagon(SDL_Renderer * renderer, int x0, int y0, int radius)
+{
+	SDL_Point hexagon[7];
+	hexagon[0].x = x0 - radius;     hexagon[0].y = y0;
+	hexagon[1].x = x0 - radius / 2; hexagon[1].y = y0 - radius * sqrt(3) / 2;
+	hexagon[2].x = x0 + radius / 2; hexagon[2].y = y0 - radius * sqrt(3) / 2;
+	hexagon[3].x = x0 + radius;     hexagon[3].y = y0;
+	hexagon[4].x = x0 + radius / 2; hexagon[4].y = y0 + radius * sqrt(3) / 2;
+	hexagon[5].x = x0 - radius / 2; hexagon[5].y = y0 + radius * sqrt(3) / 2;
+	hexagon[6].x = x0 - radius;     hexagon[6].y = y0;
+	SDL_RenderDrawLines(renderer, hexagon, 7);
+
+}
+
+void drawHexagon2(SDL_Renderer * renderer, int x0, int y0, int radius)
+{
+	SDL_Point hexagon[7];
+	hexagon[0].x = x0;                        hexagon[0].y = y0 - radius;
+	hexagon[1].x = x0 + radius * sqrt(3) / 2; hexagon[1].y = y0 - radius / 2;
+	hexagon[2].x = x0 + radius * sqrt(3) / 2; hexagon[2].y = x0 + radius / 2;;
+	hexagon[3].x = x0;                        hexagon[3].y = y0 + radius;
+	hexagon[4].x = x0 - radius * sqrt(3) / 2; hexagon[4].y = y0 + radius / 2;
+	hexagon[5].x = x0 - radius * sqrt(3) / 2; hexagon[5].y = y0 - radius / 2;
+	hexagon[6].x = x0;                        hexagon[6].y = y0 - radius;
+	SDL_RenderDrawLines(renderer, hexagon, 7);
+
+}
 
 int main(int, char**)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
-        SDL_Window* window = NULL;
-        SDL_Renderer* renderer = NULL;
+	if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+		SDL_Window* window = NULL;
+		SDL_Renderer* renderer = NULL;
 
-        if (SDL_CreateWindowAndRenderer(500, 500, 0, &window, &renderer) == 0) {
-            SDL_bool done = SDL_FALSE;
+		if (SDL_CreateWindowAndRenderer(500, 500, 0, &window, &renderer) == 0) {
+			SDL_bool done = SDL_FALSE;
 
 			/////
 			DrawCircle(250, 250, 280);
 			//////
 
-            while (!done) {
-                SDL_Event event;
+			while (!done) {
+				SDL_Event event;
 
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                SDL_RenderClear(renderer);
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+				SDL_RenderClear(renderer);
 
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                // SDL_RenderDrawLines(renderer, points, POINTS_COUNT);
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+				// SDL_RenderDrawLines(renderer, points, POINTS_COUNT);
 
 				/////
 				SDL_RenderDrawPoints(renderer, points2, cpt+1);
 				/////
 
-                SDL_RenderDrawLine(renderer, 50, 250, 450, 250); //barre horizontale
-                SDL_RenderDrawLine(renderer, 250, 50, 250, 450); //barre verticale
-				for (int j=0; j<200; j++) {
-                   SDL_SetRenderDrawColor(renderer, 50*j%255, 30*j%255, 35*j%255, SDL_ALPHA_OPAQUE);
-					MyDrawCircle(renderer, 250, 250, j);
+				drawHexagon(renderer, 250, 255, 50);
+				drawHexagon2(renderer, 150, 155, 50);
+				// drawCircles(renderer);
+
+				SDL_RenderPresent(renderer);
+
+				while (SDL_PollEvent(&event)) {
+					if (event.type == SDL_QUIT) {
+						done = SDL_TRUE;
+					}
 				}
+			}
+		}
 
-
-                SDL_RenderPresent(renderer);
-
-                while (SDL_PollEvent(&event)) {
-                    if (event.type == SDL_QUIT) {
-                        done = SDL_TRUE;
-                    }
-                }
-            }
-        }
-
-        if (renderer) {
-            SDL_DestroyRenderer(renderer);
-        }
-        if (window) {
-            SDL_DestroyWindow(window);
-        }
-    }
-    SDL_Quit();
-    return 0;
+		if (renderer) {
+			SDL_DestroyRenderer(renderer);
+		}
+		if (window) {
+			SDL_DestroyWindow(window);
+		}
+	}
+	SDL_Quit();
+	return 0;
 }
