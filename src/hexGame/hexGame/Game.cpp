e98eaea1ae222803da050e73a9d0b36551b6bcc1
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-04-21 17:07:19 +0200

    add configs, installs, symbols...

--- src/hexGame/hexGame/Game.cpp
+++ src/hexGame/hexGame/Game.cpp
@@ -1,4 +1,6 @@
 
+#include <iostream>
+#include <string>
 #include "Game.hpp"
 
 Game::Game(int boardSize):
@@ -9,4 +11,23 @@ Game::Game(int boardSize):
 
 Game::~Game()
 {
+}
+
+
+void Game::printBoard()
+{
+	std::string space = "";
+
+	std::cout << std::endl;
+	for (int i=0; i<=boardSize; i++) {
+
+		space.append(" ");
+		std::cout << space;
+		for (int j=0; j<=boardSize; j++) {
+			std::cout << "[]";
+		}
+
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
--- src/hexGame/hexGame/Game.hpp
+++ src/hexGame/hexGame/Game.hpp
@@ -1,14 +1,24 @@
 
+#pragma once
+
+#include "Board.hpp"
+#include "HexGameLib_export.hpp"
+
+// Without HEXGAMELIB_EXPORT, we get : error LNK2019 or : fatal error LNK1104
+// https://docs.microsoft.com/fr-fr/cpp/error-messages/tool-errors/linker-tools-error-lnk2019?view=msvc-160
+// https://docs.microsoft.com/fr-fr/cpp/error-messages/compiler-errors-1/fatal-error-c1083?view=msvc-160
 class Game
 {
 private:
 	int boardSize;
-	// Board
+	Board * board;
 	// Player1
 	// Player2
 	// GameUI gameUI;
 
 public:
-	Game(int boardSize);
-	~Game();
+	HEXGAMELIB_EXPORT Game(int boardSize);
+	HEXGAMELIB_EXPORT ~Game();
+
+	HEXGAMELIB_EXPORT void printBoard();
 };
