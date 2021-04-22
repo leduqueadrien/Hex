033ce2ab24a1f8e23402b74e7cc3264d13a44086
CavaniNicolas <cavani.nicolas6@gmail.com>
2021-04-22 13:45:43 +0200

    fix include quote brackets

--- src/apps/console/ConsoleUI.hpp
+++ src/apps/console/ConsoleUI.hpp
@@ -1,15 +1,14 @@
 
-#include "hexGame/GameUI.hpp"
-#include "hexGame/Board.hpp"
+#include <hexGame/GameUI.hpp>
+#include <hexGame/Board.hpp>
 
 class ConsoleUI : public GameUI
 {
 
 private:
-	/* data */
 
 public:
-	ConsoleUI(/* args */);
+	ConsoleUI();
 	~ConsoleUI();
 
 	void displayBoard(Board* board);
