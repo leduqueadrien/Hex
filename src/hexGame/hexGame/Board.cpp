e32aea40748aa3a109e437254f81474334a69ddd
adrien <aleduque@ensc.fr>
2021-04-22 23:09:22 +0200

    correction erreur creation plateau de jeu dans Board et implementaion operateur de copie de board

--- src/hexGame/hexGame/Board.cpp
+++ src/hexGame/hexGame/Board.cpp
@@ -1,31 +1,30 @@
 
 #include "Board.hpp"
+#include "Tile.hpp"
 
 
 Board::Board(int size):
     m_size(size)
 {
+    std::vector<Tile*> tmp;
     for (int i=0; i<m_size; ++i) {
         for (int j=0; j<m_size; ++j) {
-            m_board.at(i).push_back(new Tile(i, j));
+            tmp.push_back(new Tile(i, j));
         }
+        m_board.push_back(tmp);
     }
 }
 
 
 Board::~Board()
 {
+
 }
 
 
 void Board::initBoard()
 {
-    for (int i=0; i<m_size; ++i) {
-        for (int j=0; j<m_size; ++j) {
-            m_board.at(i).at(j)->setColor(Color::Undefined);
-            m_board.at(i).at(j)->setIsChecked(false);
-        }
-    }
+    deleteBoard();
 }
 
 
@@ -57,11 +56,35 @@ Tile* Board::getTile(int i, int j) const
 }
 
 
+void Board::deleteBoard() {
+    for (int i=0; i<m_size; ++i) {
+        for (int j=0; j<m_size; ++j) {
+            delete m_board.at(i).at(j);
+        }
+    }
+}
+
+
 Board& Board::operator=(const Board& board)
 {
 	if (this != &board) {
-		if (board.m_size == m_size) {
-            //copy board.board in this.board
+		if (board.m_size != m_size) {
+            m_size = board.m_size;
+            deleteBoard();
+            for (int i=0; i<m_size; ++i) {
+                std::vector<Tile *> tmp;
+                for (int j=0; j<m_size; ++j) {
+                    Tile t = *(board.m_board.at(i).at(j));
+                    tmp.push_back(new Tile(t.getI(), t.getJ(), t.getColor(), t.getIsChecked()));
+                }
+                m_board.push_back(tmp);
+            }
+        } else {
+            for (int i=0; i<m_size; ++i) {
+                for (int j=0; j<m_size; ++j) {
+                    m_board.at(i).at(j) = board.m_board.at(i).at(j);
+                }
+            }
         }
 	}
 	return *this;
