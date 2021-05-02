f4a27be47012e5d8112bdb08a001345fd1b5416b
Cavani Nicolas <cavani.nicolas6@gmail.com>
2021-05-02 14:13:45 +0200

    add documentation

--- src/apps/graphic/graphicUI/GraphicUI.hpp
+++ src/apps/graphic/graphicUI/GraphicUI.hpp
@@ -7,14 +7,40 @@ class GraphicUI final : public GameUI
 {
 
 private:
+	/**
+	 * @brief the SDL2 framework
+	 */
     Framework* m_framework;
+
+	/**
+	 * @brief margin distance between the window and the board
+	 */
     int m_margin;
 
 public:
+	/**
+	 * @brief Construct a new GraphicUI object
+	 */
 	GraphicUI();
+
+	/**
+	 * @brief Destroy the GraphicUI object
+	 */
 	~GraphicUI();
 
+	/**
+	 * @brief Display the board using SDL
+	 * 
+	 * @param board board to be displayed
+	 */
 	void displayBoard(Board* board);
+
+
+	/**
+	 * @brief Get the Player Move once the player clicked on the screen
+	 * 
+	 * @param move Move the player made
+	 */
 	void getPlayerMove(Move& move);
 
 };
