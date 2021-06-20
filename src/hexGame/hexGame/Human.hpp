3b8cac67f8c895e8e9d7b2b836b48fd152ab4410
adrien <aleduque@ensc.fr>
2021-06-20 19:25:33 +0200

    add all documentation

--- src/hexGame/hexGame/Human.hpp
+++ src/hexGame/hexGame/Human.hpp
@@ -6,16 +6,37 @@
 #include "Player.hpp"
 #include "GameUI.hpp"
 
+
+/**
+ * @brief human player class
+ */
 class Human final : public Player
 {
 
 private:
+	/**
+	 * @brief user interface of the game
+	 */
     GameUI* m_gameUI;
 
 public:
+
+	/**
+	 * @brief constructor
+	 * @param color player's color
+	 * @param gameUI user interface of the game
+	 */
 	HEXGAMELIB_EXPORT Human(Color color, GameUI* gameUI);
+
+	/**
+	 * @brief destructor
+	 */
 	HEXGAMELIB_EXPORT ~Human();
 
+	/**
+	 * @brief choose the move to make
+	 * @return Move move made 
+	 */
 	HEXGAMELIB_EXPORT Move makeMove();
 
 };
