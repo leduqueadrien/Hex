bc895b54e08d5f03549ef8a4ac7d0f158b703ad4
adrien <aleduque@ensc.fr>
2021-06-20 19:16:53 +0200

    add all documentation

--- src/hexGame/hexGame/GameUI.hpp
+++ src/hexGame/hexGame/GameUI.hpp
@@ -10,16 +10,46 @@
 // https://docs.microsoft.com/fr-fr/cpp/cpp/using-dllimport-and-dllexport-in-cpp-classes?view=msvc-160
 // https://docs.microsoft.com/fr-fr/cpp/error-messages/tool-errors/linker-tools-error-lnk2001?view=msvc-160#what-is-an-unresolved-external-symbol
 
+
+/**
+ * @brief user interface base class
+ */
 class GameUI
 {
 private:
 
 public:
+
+	/**
+	 * @brief constructor
+	 */
 	HEXGAMELIB_EXPORT GameUI() = default;
+
+	/**
+	 * @brief destructor
+	 */
 	HEXGAMELIB_EXPORT virtual ~GameUI() = default;
 
+	/**
+	 * @brief display the board
+	 */
 	HEXGAMELIB_EXPORT virtual void displayBoard(Board*) = 0;
+
+	/**
+	 * @brief display information about the turn
+	 */
 	HEXGAMELIB_EXPORT virtual void displayTurnInfo(int, Color) = 0;
-	HEXGAMELIB_EXPORT virtual void displayMove(Move) = 0;
+
+	/**
+	 * @brief display a move
+	 * 
+	 * @param move move who had to be display
+	 */
+	HEXGAMELIB_EXPORT virtual void displayMove(Move move) = 0;
+
+	/**
+	 * @brief Get the player move
+	 * @param move move made
+	 */
 	HEXGAMELIB_EXPORT virtual void getPlayerMove(Move& move) = 0;
 };
