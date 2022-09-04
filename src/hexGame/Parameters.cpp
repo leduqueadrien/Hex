6806a0cb75821898d6ccbd363fe37a6027cc3177
adrien <adrien.leduque@gmail.com>
2022-09-04 12:23:11 +0200

    Move the methods implementation from hpp file into cpp file

--- src/hexGame/Parameters.cpp
+++ src/hexGame/Parameters.cpp
@@ -2,6 +2,85 @@
 #include "Parameters.hpp"
 #include <algorithm>
 
+
+// ############################################################################
+// ########################## Parameter #######################################
+// ############################################################################
+Parameter::Parameter(const std::string &name) : m_name(name) {
+}
+
+std::string Parameter::getName() const {
+    return m_name;
+}
+
+void Parameter::setName(const std::string &name) {
+    m_name = name;
+}
+
+
+// ############################################################################
+// ########################## ParameterBoardSize ##############################
+// ############################################################################
+    
+ParameterBoardSize::ParameterBoardSize(const std::string &name, int boardSize)
+    : Parameter(name), m_boardSize(boardSize) {
+}
+
+int ParameterBoardSize::getBoardSize() {
+    return m_boardSize;
+}
+
+void ParameterBoardSize::setBoardSize(int boardSize) {
+    m_boardSize = boardSize;
+}
+
+std::string ParameterBoardSize::to_string() {
+    return "";
+}
+
+
+// ############################################################################
+// ########################## ParameterPlayer #################################
+// ############################################################################
+ParameterPlayer::ParameterPlayer(const std::string &name, Color color,
+                                 const std::string &playerType,
+                                 int monteCarloNbGame)
+    : Parameter(name), m_color(color), m_playerType(playerType),
+      m_monteCarloNbGame(monteCarloNbGame) {
+}
+
+Color ParameterPlayer::getColor() {
+    return m_color;
+}
+
+void ParameterPlayer::setColor(Color color) {
+    m_color = color;
+}
+
+std::string ParameterPlayer::getPlayerType() {
+    return m_playerType;
+}
+
+void ParameterPlayer::setPlayerType(const std::string &playerType) {
+    m_playerType = playerType;
+}
+
+int ParameterPlayer::getMonteCarloNbGame() {
+    return m_monteCarloNbGame;
+}
+
+void ParameterPlayer::setMonteCarloNbGame(int monteCarloNbGame) {
+    m_monteCarloNbGame = monteCarloNbGame;
+}
+
+std::string ParameterPlayer::to_string() {
+    return "";
+}
+
+
+// ############################################################################
+// ########################## Parameters ######################################
+// ############################################################################
 Parameters::Parameters() {
 
     std::shared_ptr<ParameterBoardSize> paramBoardSize =
@@ -32,13 +111,13 @@ Parameters::const_iterator Parameters::end() {
     return m_parameters.end();
 }
 
-ParameterBoardSize::ParameterBoardSize(const std::string &name, int boardSize)
-    : Parameter(name), m_boardSize(boardSize) {
+
+// ############################################################################
+// ########################## ComparaisonStringParameter ######################
+// ############################################################################
+ComparaisonStringParameter::ComparaisonStringParameter(const std::string &name) : m_name(name) {
 }
 
-ParameterPlayer::ParameterPlayer(const std::string &name, Color color,
-                                 const std::string &playerType,
-                                 int monteCarloNbGame)
-    : Parameter(name), m_color(color), m_playerType(playerType),
-      m_monteCarloNbGame(monteCarloNbGame) {
+bool ComparaisonStringParameter::operator()(const std::shared_ptr<Parameter> &param) {
+    return m_name == param->getName();
 }
--- src/hexGame/Parameters.hpp
+++ src/hexGame/Parameters.hpp
@@ -8,40 +8,39 @@
 #include <stdexcept>
 #include <string>
 
+// ############################################################################
+// ########################## Parameter #######################################
+// ############################################################################
 class Parameter {
 private:
     std::string m_name;
 
 public:
-    Parameter(const std::string &name) : m_name(name) {
-    }
+    Parameter(const std::string &name);
     ~Parameter() = default;
-    std::string getName() const {
-        return m_name;
-    }
-    void setName(const std::string &name) {
-        m_name = name;
-    }
+    std::string getName() const;
+    void setName(const std::string &name);
     virtual std::string to_string() = 0;
 };
 
+// ############################################################################
+// ########################## ParameterBoardSize ##############################
+// ############################################################################
 class ParameterBoardSize : public Parameter {
 private:
     int m_boardSize;
 
 public:
     ParameterBoardSize(const std::string &name, int boardSize);
-    int getBoardSize() {
-        return m_boardSize;
-    }
-    void setBoardSize(int boardSize) {
-        m_boardSize = boardSize;
-    }
-    std::string to_string() override {
-        return "";
-    }
+    int getBoardSize();
+    void setBoardSize(int boardSize);
+    std::string to_string() override;
 };
 
+
+// ############################################################################
+// ########################## ParameterPlayer #################################
+// ############################################################################
 class ParameterPlayer : public Parameter {
 private:
     Color m_color;
@@ -51,29 +50,19 @@ private:
 public:
     ParameterPlayer(const std::string &name, Color color,
                     const std::string &playerType, int monteCarloNbGame);
-    Color getColor() {
-        return m_color;
-    }
-    void setColor(Color color) {
-        m_color = color;
-    }
-    std::string getPlayerType() {
-        return m_playerType;
-    }
-    void setPlayerType(const std::string &playerType) {
-        m_playerType = playerType;
-    }
-    int getMonteCarloNbGame() {
-        return m_monteCarloNbGame;
-    }
-    void setMonteCarloNbGame(int monteCarloNbGame) {
-        m_monteCarloNbGame = monteCarloNbGame;
-    }
-    std::string to_string() override {
-        return "";
-    }
+    Color getColor();
+    void setColor(Color color);
+    std::string getPlayerType();
+    void setPlayerType(const std::string &playerType);
+    int getMonteCarloNbGame();
+    void setMonteCarloNbGame(int monteCarloNbGame);
+    std::string to_string() override;
 };
 
+
+// ############################################################################
+// ########################## Parameters ######################################
+// ############################################################################
 class Parameters {
 private:
     std::list<std::shared_ptr<Parameter>> m_parameters;
@@ -89,14 +78,15 @@ public:
     const_iterator end();
 };
 
+
+// ############################################################################
+// ########################## ComparaisonStringParameter ######################
+// ############################################################################
 class ComparaisonStringParameter {
 private:
     std::string m_name;
 
 public:
-    ComparaisonStringParameter(const std::string &name) : m_name(name) {
-    }
-    bool operator()(const std::shared_ptr<Parameter> &param) {
-        return m_name == param->getName();
-    }
+    ComparaisonStringParameter(const std::string &name);
+    bool operator()(const std::shared_ptr<Parameter> &param);
 };
